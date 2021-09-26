8aa29d8bc0d76c7637010e2caa950ff516a4bb04
ds3569 <76651279+ds3569@users.noreply.github.com>
2021-09-26 16:17:45 +0900

    Add files via upload

--- graphModule/getData.py
+++ graphModule/getData.py
@@ -21,13 +21,14 @@ def getWineData():
     
     return dateSet
 
+#사용자 리뷰 기록의 별점 별로 가산점 리스트 작성
 def setStarPoint(userInfo):
-    unReviewedList = {}
+
     userReview = list(userInfo.values())
     userReview = literal_eval(str(userReview[0]))
     userReview = list(userReview.values())
 
-    print(userReview)
+
 
     starPoint = []
 
@@ -35,44 +36,63 @@ def setStarPoint(userInfo):
         if i == 0:
             starPoint.append(0)
         elif i == 1:
-            starPoint.append(10)
+            starPoint.append(0.1)
         elif i == 2:
-            starPoint.append(30)
+            starPoint.append(0.3)
         elif i == 3:
-            starPoint.append(60)
+            starPoint.append(0.6)
         elif i == 4:
-            starPoint.append(80)
+            starPoint.append(0.8)
         else:
-            starPoint.append(100)
-    print(starPoint)
+            starPoint.append(1.0)
+
     return starPoint
 
+#리뷰한 모든 와인의 Sweet 평가
 def getsweetfavor(wineInfo, starPoint):
     favor = 0
+    num = 0
 
     for i in range(0, len(wineInfo)):
-        favor = favor + (wineInfo[i][12] * starPoint[i])
+        favor = favor + (wineInfo[i][11] * starPoint[i]) #해당 와인의 Sweet * 리뷰 가산점
+    for i in range(0, len(wineInfo)):
+        if (starPoint[i] != 0):
+            num += 1
 
+    return favor/num
 
-    return favor/len(wineInfo)
-
+#리뷰한 모든 와인의 Acidic 평가
 def getacidicfavor(wineInfo, starPoint):
     favor = 0
+    num = 0
     for i in range(0, len(wineInfo)):
-        favor = favor + (wineInfo[i][13] * starPoint[i])
+        favor = favor + (wineInfo[i][12] * starPoint[i]) #해당 와인의 Acidic * 리뷰 가산점
+    for i in range(0, len(wineInfo)):
+        if (starPoint[i] != 0):
+            num += 1
 
-    return favor/len(wineInfo)
+    return favor/num
 
+#리뷰한 모든 와인의 Body 평가
 def getbodyfavor(wineInfo, starPoint):
     favor = 0
+    num = 0
     for i in range(0, len(wineInfo)):
-        favor = favor + (wineInfo[i][14] * starPoint[i])
+        favor = favor + (wineInfo[i][13] * starPoint[i]) #해당 와인의 Body * 리뷰 가산점
+    for i in range(0, len(wineInfo)):
+        if (starPoint[i] != 0):
+            num += 1
 
-    return favor/len(wineInfo)
+    return favor/num
 
+#리뷰한 모든 와인의 Tannin 평가
 def gettanninfavor(wineInfo, starPoint):
     favor = 0
+    num = 0
     for i in range(0, len(wineInfo)):
-        favor = favor + (wineInfo[i][15] * starPoint[i])
+        favor = favor + (wineInfo[i][14] * starPoint[i]) #해당 와인의 Tannin * 리뷰 가산점
+    for i in range(0, len(wineInfo)):
+        if (starPoint[i] != 0):
+            num += 1
 
-    return favor/len(wineInfo)
+    return favor/num
--- graphModule/graphModule.py
+++ graphModule/graphModule.py
@@ -1,6 +1,7 @@
 import getData
 import getdata
 
+#와인 그래프 데이터 생성
 def getFavorList(username):
     favorList =[]
 
@@ -9,14 +10,20 @@ def getFavorList(username):
     userinfo = getdata.getUserInfo(username, dataSet)
     starPoint = getData.setStarPoint(userinfo)
 
-    sweetFavor = getData.getsweetfavor(wineSet, starPoint)
-    acidicFavor = getData.getacidicfavor(wineSet, starPoint)
-    bodyFavor = getData.getbodyfavor(wineSet, starPoint)
-    tanninFavor = getData.gettanninfavor(wineSet, starPoint)
+    if (sum(starPoint) > 0): #리뷰한 와인이 있을 경우
+        sweetFavor = getData.getsweetfavor(wineSet, starPoint)
+        acidicFavor = getData.getacidicfavor(wineSet, starPoint)
+        bodyFavor = getData.getbodyfavor(wineSet, starPoint)
+        tanninFavor = getData.gettanninfavor(wineSet, starPoint)
 
-    favorList.append(sweetFavor)
-    favorList.append(acidicFavor)
-    favorList.append(bodyFavor)
-    favorList.append(tanninFavor)
+        favorList.append(sweetFavor)
+        favorList.append(acidicFavor)
+        favorList.append(bodyFavor)
+        favorList.append(tanninFavor)
 
-    return favorList
+        return favorList
+    else: #리뷰한 와인이 없을 경우
+        return [0, 0, 0, 0]
+
+
+
